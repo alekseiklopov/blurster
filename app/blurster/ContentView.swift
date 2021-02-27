ca825f4116772518449251e590ffb0a6707908e6
Aleksei Klopov <34196081+alekseiklopov@users.noreply.github.com>
2021-02-27 20:43:29 +0300

    Init iOS app

---
+++ app/blurster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  blurster
+//
+//  Created by Aleksei Klopov on 27.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app/blurster/blursterApp.swift
@@ -0,0 +1,17 @@
+//
+//  blursterApp.swift
+//  blurster
+//
+//  Created by Aleksei Klopov on 27.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct blursterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
