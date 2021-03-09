6e5722e4ac3da3e21d8388981d0781e2cbda3b01
Aleksei Klopov <34196081+alekseiklopov@users.noreply.github.com>
2021-03-09 20:45:12 +0300

    Add full backgound blurring

--- Blurster/Blurster/UIImage+Helpers.swift
+++ Blurster/Blurster/UIImage+Helpers.swift
@@ -9,20 +9,75 @@ import UIKit
 import CoreML
 
 extension UIImage {
-
+    
     // MARK: - Image predictions
     func getBlurred() -> UIImage? {
+        
+        // Set a size for making predictions using the ML model
+        let n = 513
+        
+        // Define helpers
+        
+        // Create an n-by-n mask
+        func createMask(array: MLMultiArray) -> CIImage? {
+            var imageArray: [UInt8] = []
+            for i in 0 ..< n * n {
+                // Create a mask only for the people in the image
+                if array[i] == 15 {
+                    imageArray.append(255)
+                } else {
+                    imageArray.append(0)
+                }
+            }
+            let data = Data(imageArray)
+            let newImage = CIImage(
+                bitmapData: data,
+                bytesPerRow: n,
+                size: CGSize(width: n, height: n),
+                format: CIFormat(rawValue: CIFormat
+                                    .RawValue(kCVPixelFormatType_1Monochrome)),
+                colorSpace: nil)
+            return newImage
+        }
+        
         do {
-            let squaredImage = self.squareWithPadding(toSize: 513)
-            let imageBuffer = self.buffer(from: squaredImage!)
+            // Prepare the image for ML model
+            guard let squaredImage = self.squareWithPadding(toSize: n),
+                  let imageBuffer = self.buffer(from: squaredImage)
+            else { return nil }
+            
+            // Predict results
             let model = try DeepLabV3(configuration: .init())
             let prediction = try model
-                .prediction(image: imageBuffer!)
+                .prediction(image: imageBuffer)
                 .semanticPredictions
-            print(prediction)
-            // TODO: Blur image background using the prediction array
             
-            return self
+            // Create, crop and resize the mask to initial image size
+            let w = self.size.width, h = self.size.height
+            var squaredImageHeight = Int(CGFloat(n) * h / w)
+            var squaredImageWidth = n
+            var sizeCoefficient = w / CGFloat(n)
+            if h > w {
+                squaredImageHeight = n
+                squaredImageWidth = Int(CGFloat(n) * w / h)
+                sizeCoefficient = h / CGFloat(n)
+            }
+            guard let maskCIImage = createMask(array: prediction)?
+                    .cropped(to: CGRect(x: 0,
+                                        y: 0,
+                                        width: squaredImageWidth,
+                                        height: squaredImageHeight))
+                    .scale(sizeCoefficient)
+            else { return nil }
+            
+            // Blur image background
+            guard let ciImage = CIImage(image: self),
+                  let outputImage = ciImage
+                    .blurWith(mask: maskCIImage)?
+                    .toUIImage()
+            else { return nil }
+            
+            return outputImage
         } catch {
             print(error)
         }
@@ -31,9 +86,10 @@ extension UIImage {
     
     // MARK: - Image utilities
     // TODO: Let the user crop their image to a square
-    func squareWithPadding(toSize: Int) -> UIImage? {
+    private func squareWithPadding(toSize: Int) -> UIImage? {
         // First place the image into a square of maximum size of the image,
-        // then resize it to new size
+        // then resize it to new size.
+        // It's necessary to process an unscaled image.
         let w = self.size.width, h = self.size.height
         let max_size = max(w, h)
         UIGraphicsBeginImageContextWithOptions(
@@ -45,7 +101,7 @@ extension UIImage {
         return newImage?.resize(toSize: CGSize(width: toSize, height: toSize))
     }
     
-    func resize(toSize: CGSize) -> UIImage? {
+    private func resize(toSize: CGSize) -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(toSize, false, 0.0)
         self.draw(in: CGRect(x: 0,
                              y: 0,
@@ -56,7 +112,7 @@ extension UIImage {
         return newImage
     }
     
-    func buffer(from image: UIImage) -> CVPixelBuffer? {
+    private func buffer(from image: UIImage) -> CVPixelBuffer? {
         let attrs = [
             kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
             kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue
@@ -69,9 +125,7 @@ extension UIImage {
             kCVPixelFormatType_32ARGB,
             attrs,
             &pixelBuffer)
-        guard (status == kCVReturnSuccess) else {
-            return nil
-        }
+        guard (status == kCVReturnSuccess) else { return nil }
         CVPixelBufferLockBaseAddress(pixelBuffer!,
                                      CVPixelBufferLockFlags(rawValue: 0))
         let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
